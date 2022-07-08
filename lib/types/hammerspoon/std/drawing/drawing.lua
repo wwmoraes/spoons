e1135d75b13a3130d6d9cedfea43e44f4c2ed5fa
William Artero <github@artero.dev>
2022-07-08 19:36:23 +0200

    docs: added hs.drawing windowBehaviors type

--- lib/types/hammerspoon/std/drawing/drawing.lua
+++ lib/types/hammerspoon/std/drawing/drawing.lua
@@ -2,8 +2,24 @@
 
 ---@deprecated
 ---@class HSDrawing
+---@field windowBehaviors WindowBehaviors
 local HSDrawing
 
 ---@deprecated
 ---@class Drawing
 local Drawing
+
+---@class WindowBehaviors
+---@field [string] number
+---@field default 0
+---@field canJoinAllSpaces 1
+---@field moveToActiveSpace 2
+---@field managed 4
+---@field transient 8
+---@field stationary 16
+---@field fullScreenPrimary 28
+---@field participatesInCycle 32
+---@field fullScreenAllowsTiling 48
+---@field fullScreenAuxiliary 56
+---@field ignoresCycle 64
+---@field fullScreenDisallowsTiling 96
