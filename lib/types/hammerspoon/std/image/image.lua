07ef668e368680de520d3c332b2d649dbdc326ef
William Artero <github@artero.dev>
2022-07-12 00:16:06 +0200

    docs: added hs.image typings

--- lib/types/hammerspoon/std/image/image.lua
+++ lib/types/hammerspoon/std/image/image.lua
@@ -1,4 +1,59 @@
--- // TODO hs.image
-
---- @class HSImage
+---@class HSImage
+---@field additionalImageNames AdditionalImageNames
+---@field systemImageNames SystemImageNames
 local HSImage
+
+---@param path string
+---@return table? -- // TODO EXIF table
+function HSImage.getExifFromPath(path) end
+
+---@param file string | string[]
+---@return Image?
+function HSImage.iconForFile(file) end
+
+---@param fileType string
+---@return Image?
+function HSImage.iconForFileType(fileType) end
+
+---@param bundleID string
+---@return Image?
+function HSImage.imageFromAppBundle(bundleID) end
+
+---@param ascii string
+---@param context? ContextOptions[]
+---@return Image?
+function HSImage.imageFromASCII(ascii, context) end
+
+---@class ContextOptions
+-- the color with which the shape will be filled (defaults to black)
+---@field fillColor? RGBColor
+-- the color with which the shape will be stroked (defaults to black)
+---@field strokeColor RGBColor
+-- the line width for the stroke of the shape (defaults to 1 if anti-aliasing is
+-- on or (√2)/2 if it is off - approximately 0.7)
+---@field lineWidth number
+-- a boolean indicating whether or not the shape should be closed
+-- (defaults to `true`)
+---@field shouldClose boolean
+-- a boolean indicating whether or not the shape should be antialiased
+-- (defaults to `true`)
+---@field antialias boolean
+
+---@param file string
+---@return Image?
+function HSImage.imageFromMediaFile(file) end
+
+---@param name string
+---@return Image?
+function HSImage.imageFromName(name) end
+
+---@param path string
+---@return Image?
+function HSImage.imageFromPath(path) end
+
+---@alias ImageCallback fun(image: Image) -- // TODO confirm callback argument
+
+---@param url string
+---@param callbackFn? ImageCallback
+---@return Image?
+function HSImage.imageFromURL(url, callbackFn) end
--- lib/types/hammerspoon/std/image/instance.lua
+++ lib/types/hammerspoon/std/image/instance.lua
@@ -1,4 +1,63 @@
--- // TODO hs.image
-
 --- @class Image
 local Image
+
+---@param size? Size
+---@param gray? boolean
+---@return Image
+function Image:bitmapRepresentation(size, gray) end
+
+---@param point Point
+---@return DrawingColor
+function Image:colorAt(point) end
+
+---@return Image
+function Image:copy() end
+
+---@param rectangle Rectangle
+---@return Image
+function Image:croppedCopy(rectangle) end
+
+---@param scale? boolean
+---@param type? ImageType
+---@return string
+function Image:encodeAsURLString(scale, type) end
+
+---@param name string
+---@return Image?
+---@overload fun(): string
+function Image:name(name) end
+
+---@param filename string
+---@param scale? boolean
+---@param filetype? ImageType
+---@return boolean
+function Image:saveToFile(filename, scale, filetype) end
+
+---@deprecated
+---@param name string
+---@return boolean
+---@overload fun(): string
+function Image:setName(name) end
+
+---@deprecated
+---@param size Size
+---@param absolute? boolean
+---@return Image
+function Image:setSize(size, absolute) end
+
+---@param size Size
+---@param absolute? boolean
+---@return Image
+---@overload fun(): Size
+function Image:size(size, absolute) end
+
+---@param state boolean
+---@return Image
+---@overload fun(): boolean
+function Image:template(state) end
+
+---@param width number
+---@param height number
+---@return string
+---@overload fun(): string
+function Image:toASCII(width, height) end
