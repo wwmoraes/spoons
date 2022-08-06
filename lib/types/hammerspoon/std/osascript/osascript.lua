4833c516fbd8ce80be64262b67819fa987794b06
William Artero <github@artero.dev>
2022-08-06 18:28:04 +0200

    feat: added hs.osascript typings

--- lib/types/hammerspoon/std/osascript/osascript.lua
+++ lib/types/hammerspoon/std/osascript/osascript.lua
@@ -1,4 +1,34 @@
--- // TODO hs.osascript
-
 ---@class HSOSAScript
 local HSOSAScript
+
+-- Runs osascript code
+---@param source string
+---@param language? "'AppleScript'" | "'JavaScript'"
+---@return boolean success
+---@return table? output
+---@return string raw
+function HSOSAScript._osascript(source, language) end
+
+---@param source string
+---@return boolean success
+---@return table? output
+---@return string|table raw
+function HSOSAScript.applescript(source) end
+
+---@param fileName string
+---@return boolean success
+---@return table? output
+---@return string|table raw
+function HSOSAScript.applescriptFromFile(fileName) end
+
+---@param source string
+---@return boolean success
+---@return table? output
+---@return string|table raw
+function HSOSAScript.javascript(source) end
+
+---@param fileName string
+---@return boolean success
+---@return table? output
+---@return string|table raw
+function HSOSAScript.javascriptFromFile(fileName) end
