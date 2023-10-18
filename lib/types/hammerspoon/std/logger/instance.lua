26270d670afce141b6764f6b94da2d2fcec06d22
William Artero <git@artero.dev>
2023-10-18 15:42:49 +0200

    docs: any vararg for the logger methods

--- lib/types/hammerspoon/std/logger/instance.lua
+++ lib/types/hammerspoon/std/logger/instance.lua
@@ -3,30 +3,30 @@
 local Logger
 
 --- logs debug info to the console
---- @vararg string
+--- @vararg any
 function Logger.d(...) end
 
 --- logs formatted debug info to the console
 --- @param fmt string @formatting string as per string.format
---- @vararg string
+--- @vararg any
 function Logger.df(fmt, ...) end
 
 --- logs error to the console
---- @vararg string
+--- @vararg any
 function Logger.e(...) end
 
 --- logs formatted error to the console
 --- @param fmt string @formatting string as per string.format
---- @vararg string
+--- @vararg any
 function Logger.ef(fmt, ...) end
 
 --- logs formatted info to the console
 --- @param fmt string @formatting string as per string.format
---- @vararg string
+--- @vararg any
 function Logger.f(fmt, ...) end
 
 --- logs info to the console
---- @vararg string
+--- @vararg any
 function Logger.i(...) end
 
 --- gets the log level of the logger instance
@@ -38,19 +38,19 @@ function Logger.getLoggerLevel() end
 function Logger.setLoggerLevel(loglevel) end
 
 --- logs verbose info to the console
---- @vararg string
+--- @vararg any
 function Logger.v(...) end
 
 --- logs formatted verbose info to the console
 --- @param fmt string @formatting string as per string.format
---- @vararg string
+--- @vararg any
 function Logger.vf(fmt, ...) end
 
 --- logs warning to the console
---- @vararg string
+--- @vararg any
 function Logger.w(...) end
 
 --- logs formatted warning to the console
 --- @param fmt string @formatting string as per string.format
---- @vararg string
+--- @vararg any
 function Logger.wf(fmt, ...) end
